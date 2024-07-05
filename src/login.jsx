c983213111c8ada452b652b3a2ea9cc28967a3cf
marquina2014 <luiseduardo15097@gmail.com>
2024-07-05 16:26:35 -0400

    Commit para añadir el login, gabooooooooooooooooo

--- src/login.jsx
+++ src/login.jsx
@@ -1,13 +1,51 @@
-import React from 'react';
-import './Panel.css';
+import React, { useState } from 'react';
+import './Login.css';
 
 function Login() {
+    const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes manejar el envío de los datos del formulario
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
   return (
     <div className="panel">
     <div className="header">
       <i className="icon-lock"></i>
       <span>LOGIN</span>
       
+      
+    </div>
+    <div className="content">
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="input-group">
+          <label htmlFor="username">Nombre</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Contraseña</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Enviar</button>
+      </form>
+      </div>
     </div>
   </div>
   );
