5b88504eda2e0210b7b94cc6439b53ed6791d05d
marquina2014 <luiseduardo15097@gmail.com>
2024-07-11 16:37:48 -0400

    Commit para agregar el login

--- src/Panel.jsx
+++ src/Panel.jsx
@@ -1,10 +1,44 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './Panel.css';
-import background from './assets/bhp.png'
+import background from './assets/bhp.png';
 
 function Panel(props) {
   const [file, setFile] = useState(null);
   const fileInputRef = useRef(null);
+  const [areas, setAreas] = useState([]);
+
+  useEffect(() => {
+    const fetchAreas = async () => {
+      try {
+        const response = await fetch('https://prod2-24.brazilsouth.logic.azure.com:443/workflows/dee3e41beb7242f58928d11c65d2470b/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=SPKH1WvfZzx6B9SrlwkXPXrbmbNGNJjJbFsEHbWvjLo', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({})
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log('Fetch response data:', data); // Log para depuración
+
+        if (data.ok) {
+          const areasValues = data.areasPermitidas.map(area => area.Value);
+          console.log('Areas permitidas:', areasValues); // Log para depuración
+          setAreas(areasValues);
+        } else {
+          alert('Error fetching areas');
+        }
+      } catch (error) {
+        console.error('Error fetching areas:', error);
+      }
+    };
+
+    fetchAreas();
+  }, []);
 
   const handleFileChange = (event) => {
     const selectedFile = event.target.files[0];
@@ -42,7 +76,7 @@ function Panel(props) {
   };
 
   const handleLoginClick = () => {
-    props.changeScreen('Login')
+    props.changeScreen('Login');
   };
 
   return (
@@ -57,48 +91,45 @@ function Panel(props) {
       </div>
       <div className="content">
         <div className='Caja'>
-        <h2>Cargar Archivo de Pre-Validación de HH</h2>
-        <div className="form-group">
-          <label htmlFor="area-select">Selecciona el Área</label>
-          <select id="area-select" className="select">
-            <option>Catodos</option>
-            <option>Mina</option>
-            <option>NPI&CHO</option>
-            <option>Planning&Governance</option>
-            <option>Transversales</option>
-            <option>Concentradoras</option>
-          </select>
-        </div>
-        <div 
-          className="form-group file-upload" 
-          onDrop={handleDrop} 
-          onDragOver={handleDragOver}
-        >
-          <label>Cargue un Archivo excel con el formato correcto *</label>
-          <div className="file-upload-content" onClick={handleClick}>
-            <input 
-              type="file" 
-              ref={fileInputRef} 
-              style={{ display: 'none' }} 
-              onChange={handleFileChange} 
-              accept=".xls,.xlsx"
-            />
-            {file ? (
-              <p>{file.name}</p>
-            ) : (
-              <p>Sin Archivos.</p>
-            )}
-            <button className="file-button" onClick={handleFileButtonClick}>
-              <i className="icon-attach"></i>
-              Cargar Archivo
-            </button>
+          <h2>Cargar Archivo de Pre-Validación de HH</h2>
+          <div className="form-group">
+            <label htmlFor="area-select">Selecciona el Área</label>
+            <select id="area-select" className="select">
+              {areas.map((area, index) => (
+                <option key={index} value={area}>{area}</option>
+              ))}
+            </select>
           </div>
-        </div>
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" className="download-link" target="_blank" rel="noopener noreferrer">
-          Para descargar la plantilla con el formato correcto haga clic en este enlace
-        </a>
-        <button className="submit-button" onClick={handleLoginClick}>Enviar</button>
+          <div 
+            className="form-group file-upload" 
+            onDrop={handleDrop} 
+            onDragOver={handleDragOver}
+          >
+            <label>Cargue un Archivo excel con el formato correcto *</label>
+            <div className="file-upload-content" onClick={handleClick}>
+              <input 
+                type="file" 
+                ref={fileInputRef} 
+                style={{ display: 'none' }} 
+                onChange={handleFileChange} 
+                accept=".xls,.xlsx"
+              />
+              {file ? (
+                <p>{file.name}</p>
+              ) : (
+                <p>Sin Archivos.</p>
+              )}
+              <button className="file-button" onClick={handleFileButtonClick}>
+                <i className="icon-attach"></i>
+                Cargar Archivo
+              </button>
+            </div>
           </div>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" className="download-link" target="_blank" rel="noopener noreferrer">
+            Para descargar la plantilla con el formato correcto haga clic en este enlace
+          </a>
+          <button className="submit-button" onClick={handleLoginClick}>Enviar</button>
+        </div>
       </div>
     </div>
   );
--- src/login.jsx
+++ src/login.jsx
@@ -1,32 +1,92 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Login.css';
-import background from './assets/bhp.png'
+import background from './assets/bhp.png';
 
 function Login(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const userName = "admin"; // Username 
-  const passwordCorrect = '1234'; // Password 
+  const [loading, setLoading] = useState(false);
+  const [loggingIn, setLoggingIn] = useState(false); // Estado para manejar el mensaje de "Entrando..."
+  const [aid, setAid] = useState(null);
 
-  const handleSubmit = (e) => {
+  useEffect(() => {
+    const params = new URLSearchParams(document.location.search);
+    const getAID = params.get('aid');
+    setAid(getAID);
+
+    fetch(
+      "https://prod2-08.brazilsouth.logic.azure.com:443/workflows/2b574b1aaa414db19f733f218d1a9560/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=eDolAiazNNPeOvUFFbPmPTCE2LWX8W0MDCtAv3SaSbk",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          method: "post",
+          controller: "Login",
+          data: `{aid: '${getAID}'}`
+        })
+      }
+    ).then(response => {
+      if (response.status === 200) {
+        response.json().then(data => {
+          // Manejar los datos de la respuesta según sea necesario
+          setLoading(false);
+        });
+      } else {
+        // Aquí puedes manejar la respuesta no exitosa, por ejemplo mostrando un mensaje de error
+        console.error('Error verificando AID:', response.statusText);
+      }
+
+    }).catch(error => {
+      console.error('Error en la solicitud de verificación de AID:', error);
+    });
+  }, []);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    //valida que los campos no esten vacios
-    if(username === '' || password === ''){
-      alert("Debe rellenar todos los campos")
-    }
-    
-    else if(username === userName && password === passwordCorrect){
-      alert("Si es la Contraseña");
-      props.changeScreen('Panel')
-  
-    // Aquí puedes manejar el envío de los datos del formulario
-    console.log('Username:', username);
-    console.log('Password:', password);
-  }
-  else{
-    alert("Contraseña Incorrecta chaval, escribela bien");
-  }
 
+    if (username === '' || password === '') {
+      alert("Debe rellenar todos los campos");
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const response = await fetch(
+        "https://prod2-24.brazilsouth.logic.azure.com:443/workflows/dee3e41beb7242f58928d11c65d2470b/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=SPKH1WvfZzx6B9SrlwkXPXrbmbNGNJjJbFsEHbWvjLo",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            method: "post",
+            controller: "Login",
+            data: `{user: '${username}', pwd: '${password}', aid: '${aid}'}`
+          })
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Respuesta del servidor:', data);
+        setLoading(false);
+        setLoggingIn(true); // Muestra mensaje de "Entrando..."
+        setTimeout(() => {
+          props.changeScreen('Panel'); // Cambia de pantalla después de un breve retraso para mostrar el mensaje
+        }, 2500); // Retraso de 2.5 segundos
+      } else {
+        console.error('Error en la solicitud:', response.statusText);
+        alert("Contraseña Incorrecta");
+        setLoading(false);
+      }
+    } catch (error) {
+      console.error('Error en la solicitud:', error);
+      alert("Error en la solicitud.");
+      setLoading(false);
+    }
   };
 
   return (
@@ -35,43 +95,47 @@ function Login(props) {
         backgroundImage: `URL(${background})`
       }}
     >
-    <div className="header2">
-      <i className="icon-lock2"></i>
-      <span>Login</span>
-      
-      
-    </div>
-    <div className="content2">
-    <div className="login-container2">
-      <h2>Inicio De Sesión</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="input-group2">
-          <label htmlFor="username">Nombre</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
+      <div className="header2">
+        <i className="icon-lock2"></i>
+        <span>Login</span>
+      </div>
+      <div className="content2">
+        <div className="login-container2">
+          <h2>Inicio De Sesión</h2>
+          {loggingIn ? (
+            <div className="entrando-message">Entrando...</div>
+          ) : (
+            <form onSubmit={handleSubmit}>
+              <div className="input-group2">
+                <label htmlFor="username">Nombre</label>
+                <input
+                  type="text"
+                  id="username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="input-group2">
+                <label htmlFor="password">Contraseña</label>
+                <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="button-container2">
+                <button type="submit" className="submit" disabled={loading}>
+                  {loading ? 'Enviando...' : 'Enviar'}
+                </button>
+              </div>
+            </form>
+          )}
         </div>
-        <div className="input-group2">
-          <label htmlFor="password">Contraseña</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <div className="button-container2">
-        <button className="submit" onClick={handleSubmit}>Enviar</button>
-        </div>
-      </form>
       </div>
     </div>
-  </div>
   );
 }
 
